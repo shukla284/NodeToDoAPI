e5a1e7572cf10311a03329c101a404d7ca1a9583
DESKTOP-GHULTST\hp <www.shivamshukla284@outlook.com>
2020-05-05 23:54:45 +0530

    Added new parser

--- server/db/mongooseConfig.js
+++ server/db/mongooseConfig.js
@@ -1,8 +1,17 @@
 // imports and database configuration
 const mongoose = require('mongoose');
-const dbURL = (process.env.MONGODB_URI) || ('mongodb://localhost:27017/ToDoApp');
+const dbURL = (process.env.MONGODB_URI)  || ('mongodb://localhost:27017/ToDoApp');
 
 mongoose.Promise = global.Promise;
 
 // connection being initiated
-mongoose.connect(dbURL);
+//mongoose.connect(dbURL);
+
+mongoose.connect(dbURL, {
+        useNewUrlParser: true
+    }, function(err, db) {
+        if (err)
+           return console.log('Error while connecting ', err);
+        console.log('Successful connection');
+    }
+);
