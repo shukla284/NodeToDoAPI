160c9d7dc91a8f46ec1063bceda112bdecddf493
DESKTOP-GHULTST\hp <www.shivamshukla284@outlook.com>
2020-05-06 00:56:06 +0530

    Corrected OR symbol

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 
-var port = process.env.PORT | 3000;
+var port = process.env.PORT || 3000;
 
 var {mongoose} = require('./db/mongooseConfig');
 var {ToDoModel} = require('./models/ToDo');
